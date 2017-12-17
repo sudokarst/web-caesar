2ae7822470decbeea69a630832db2277bb76ee84
sudokarst <sudokarst@gmail.com>
2017-12-16 20:03:04 -0500

    phase 2 complete: returns encrypted text in form; roundtrip is free!

--- main.py
+++ main.py
@@ -10,27 +10,26 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <form method="POST">
             <label for="rotate-by">Rotate by:<label>
-            <input id="rotate-by" type="text" name="rot">
-            <textarea name="text">
-            </textarea>
+            <input id="rotate-by" type="text" name="rot" value="{rot}">
+            <textarea name="text">{text}</textarea>
             <input type="submit" value="submit">
         </form>
     </body>
@@ -39,17 +38,17 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    rot = 0
+    text = "Your plaintext secret here . . ."
+    return form.format(**locals())
 
 @app.route("/", methods=['POST'])
 def encrypt():
     rot = int(request.form['rot'])
-    plaintext = request.form['text']
-    return """
-        <h1>
-            {}
-        </h1>
-        """.format(rotate_string(plaintext, rot))
+    text = request.form['text']
+    text = rotate_string(text, rot)
+    rot = 26 - (rot % 26)
+    return form.format(**locals())
 
 
 app.run()
